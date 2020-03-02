7080e1ff7f5a52beff037a5768416927f6cac8b7
sinsys <nicholas.hazel@gmail.com>
2020-03-02 12:46:41 -0800

    Readying for deploy

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DB_URL || 'postgresql://dunder_mifflin:test@localhost/noteful',
+  DATABASE_URL: process.env.DB_URL || 'postgresql://dunder_mifflin:test@localhost/noteful',
+  TEST_DATABASE_URL: 'postgresql://dunder_mifflin:test@localhost/noteful_test'
 }
